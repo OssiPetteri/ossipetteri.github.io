324b57b56ff91b65ecc36cda70616921a6b50ec6
OssiPetteri <jyri.tiusanen@hotmail.com>
2025-03-19 13:46:14 +0200

    JS testailua

---
+++ jstesti.js
@@ -0,0 +1,10 @@
+// This is a basic JavaScript file
+
+// Function to display a greeting message
+function greet() {
+    console.log("Hello, welcome to your JavaScript file!");
+}
+
+// Call the function
+greet();
+
