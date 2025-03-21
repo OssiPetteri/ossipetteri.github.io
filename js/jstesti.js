0ff35cd963779c461a367bde6d9b4dada2fefc32
OssiPetteri <jyri.tiusanen@hotmail.com>
2025-03-21 14:10:34 +0200

    kikkailua

--- js/jstesti.js
+++ js/jstesti.js
@@ -5,6 +5,10 @@ function greet() {
     console.log("Hello, welcome to your JavaScript file!");
 }
 
+function ulko() {
+    console.log("Tämä on ulkoisen JavaScript-tiedoston funktio:)");
+    document.getElementById("ulkohaku").innerHTML = "Tämä on ulkoisen JavaScript-tiedoston funktio :)";
+}
 // Call the function
 greet();
 
